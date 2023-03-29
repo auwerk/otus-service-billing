155b82faa8de48e20ac9cb096449129f5ee2f5e2
auwerk <noisefront@yandex.ru>
2023-03-29 17:10:55 +0300

    Fixed mapper injection

--- src/main/java/org/auwerk/otus/arch/billingservice/mapper/AccountMapper.java
+++ src/main/java/org/auwerk/otus/arch/billingservice/mapper/AccountMapper.java
@@ -2,10 +2,11 @@ package org.auwerk.otus.arch.billingservice.mapper;
 
 import org.auwerk.otus.arch.billingservice.api.dto.AccountDto;
 import org.auwerk.otus.arch.billingservice.domain.Account;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "cdi", uses = OperationMapper.class)
+@Mapper(componentModel = "cdi", uses = OperationMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AccountMapper {
-    
+
     AccountDto toDto(Account account);
 }
